1942a1cbdbe5e37e9f3cfcb97490466fb77d31fc
Shivanjali Mali <shivanjali7266@gmail.com>
2026-03-13 09:56:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function fetech1(){
     console.log("this is my first feature");
+    console.log("this is my second features");
 }
 function fetch2(){
     console.log("this is my second features");
