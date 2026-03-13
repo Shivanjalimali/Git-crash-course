b6060a9540abcaa88e6fe17cefe16c3df1ddde92
Shivanjalimali <Shivanjali7266@gmail.com>
2026-03-13 08:31:30 +0530

    new features in shivani folder is added

---
+++ shivani.js
@@ -0,0 +1,12 @@
+function feature1()
+{
+    console.log("This is feature 1 here....");
+}
+function feature2()
+{
+    console.log("This is feature 2 here....");
+}
+function feature3()
+{
+    console.log("This is feature 3 here....");
+}
